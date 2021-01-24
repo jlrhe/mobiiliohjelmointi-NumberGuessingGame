cad887446f2304a15a30be12587588278ef4cb8a
jlrhe <jan@heikel.fi>
2021-01-24 18:08:58 +0200

    tehtävä suoritettu

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [secretNumber, setSecretNumber] = useState('');
+  const [guessedNumber, setGuessedNumber] = useState('');
+  const [status, setStatus] = useState('No guesses yet');
+  const [guesses, setGuesses] = useState(0);
+  const [gameStopped, setGameStopped] = useState(true);
+
+  const ref_guessInput = useRef()
+
+  const randomizeSecretNumber = () => {
+    setSecretNumber(Math.floor(Math.random() * 100 + 1))
+  };
+  const guessNumber = (number) => {
+    setGuessedNumber(parseInt(number))
+  };
+  const checkGuess = () => {
+    setGuesses(guesses + 1);
+    if (guessedNumber < secretNumber) {
+      setStatus('Your guess ' + guessedNumber + ' is too low');
+    }else if (guessedNumber > secretNumber) {
+      setStatus('Your guess ' + guessedNumber + ' is too high');
+    }else if (guessedNumber === secretNumber) {
+      setStatus('Secret number was ' + secretNumber);
+      alert('Your guess ' + guessedNumber + ' is correct. You needed ' + guesses + ' guesses');
+      setGameStopped(true);
+      ref_guessInput.current.clear();
+    }
+  }
+  const reset = () => {
+    randomizeSecretNumber();
+    setGuessedNumber(0);
+    setStatus('No guesses yet');
+    setGuesses(0);
+    setGameStopped(false);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Guess a whole number in the range 1 - 100</Text>
+      <Button onPress={reset} title='Start/reset'/>
+      <TextInput 
+        style={styles.guessInput}
+        keyboardType='numeric'
+        onChangeText={number => guessNumber(number)}
+        ref={ref_guessInput}
+      />
+      <Button disabled={gameStopped} onPress={checkGuess} title='Make Guess'/>
+      <Text>{status}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +61,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  guessInput: {
+    width: 200, 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    marginVertical: 10,
   },
 });
